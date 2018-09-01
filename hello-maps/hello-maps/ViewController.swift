65da34dfc469c7921bacfaed7c2c76dc2046fcaf
Michael Chapman <submariner100@gmail.com>
2018-09-01 17:27:05 +0100

    second commit

--- hello-maps/hello-maps/ViewController.swift
+++ hello-maps/hello-maps/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+	
+	@IBOutlet weak var mapView: MKMapView!
+	@IBOutlet weak var mapTypeSegmentedControl: UISegmentedControl!
+	
+	private let locationManager = CLLocationManager()
+	
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+	self.mapView.delegate = self
+		
+	locationManager.delegate = self
+	locationManager.requestWhenInUseAuthorization()
+	locationManager.desiredAccuracy = kCLLocationAccuracyBest
+	locationManager.distanceFilter = kCLDistanceFilterNone
+	locationManager.startUpdatingLocation()
+		
+	self.mapView.showsUserLocation = true
+	self.mapTypeSegmentedControl.addTarget(self, action: #selector(mapTypeChanged), for: .valueChanged)
+	
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	@objc func mapTypeChanged(segmentedControl: UISegmentedControl) {
+		
+		switch(segmentedControl.selectedSegmentIndex) {
+			
+		case 0:
+			self.mapView.mapType = .standard
+		case 1:
+			self.mapView.mapType = .satellite
+		case 2:
+			self.mapView.mapType = .hybrid
+		default:
+			self.mapView.mapType = .standard
+			
+		}
 	}
-
-
+	
+	func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+		
+		let region = MKCoordinateRegion(center: mapView.userLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.008, longitudeDelta: 0.008))
+		
+		mapView.setRegion(region, animated: true)
+	}
 }
 
